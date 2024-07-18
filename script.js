d566ad8153c7bfa7e644697ed98aa7494cbe2592
p4n1k0 <gabre.tarick@gmail.com>
2024-07-18 18:56:59 -0300

    add cor aos circulos

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const colorToGuess = document.getElementById('rgb-color');
 const colorList = document.getElementById('color-options');
+const answer = document.getElementById('answer');
+const colors = document.getElementsByClassName('ball');
 
-function createColorCircle () {
-    for (let i =0; i < 6; i += 1) {
+function createColorCircle() {
+    for (let i = 0; i < 6; i += 1) {
         const options = document.createElement('li');
         options.className = 'ball';
         colorList.appendChild(options);
@@ -10,3 +12,11 @@ function createColorCircle () {
 }
 
 createColorCircle();
+
+function colorizeCircle() {
+    for (let i = 0; i < colors.length; i += 1) {
+        const randomRGB = () => Math.floor(Math.random() * 256);
+        colors[i].style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+    }
+}
+colorizeCircle();
